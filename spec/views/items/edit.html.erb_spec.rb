9959d87b87d94b7504984d4abeeb514259c3ebc0
balerum03 <adafer123.angra@gmail.com>
2020-11-17 18:55:02 -0600

    added active_storage, and items and groups migration files

---
+++ spec/views/items/edit.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "items/edit", type: :view do
+  before(:each) do
+    @item = assign(:item, Item.create!(
+      user: nil,
+      name: "MyString",
+      amount: 1
+    ))
+  end
+
+  it "renders the edit item form" do
+    render
+
+    assert_select "form[action=?][method=?]", item_path(@item), "post" do
+
+      assert_select "input[name=?]", "item[user_id]"
+
+      assert_select "input[name=?]", "item[name]"
+
+      assert_select "input[name=?]", "item[amount]"
+    end
+  end
+end
---
+++ spec/views/items/new.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "items/new", type: :view do
+  before(:each) do
+    assign(:item, Item.new(
+      user: nil,
+      name: "MyString",
+      amount: 1
+    ))
+  end
+
+  it "renders new item form" do
+    render
+
+    assert_select "form[action=?][method=?]", items_path, "post" do
+
+      assert_select "input[name=?]", "item[user_id]"
+
+      assert_select "input[name=?]", "item[name]"
+
+      assert_select "input[name=?]", "item[amount]"
+    end
+  end
+end
