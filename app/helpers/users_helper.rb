533464ba5ce5d9f8770fda2664d5e8aa1f9a7f89
balerum03 <adafer123.angra@gmail.com>
2020-11-25 23:20:28 -0600

    Added total amount at the top of all my items page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def total_items
+    total = Item.where(author_id: current_user.id)
+    total.each do |item|
+      if item.groups.empty?
+        total.delete(item)
+      end
+    end
+    total.sum(:amount)
+  end
 end
