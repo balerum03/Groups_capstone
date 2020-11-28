0102d20e1c1373b1f816906c0d47395d042d6a46
balerum03 <adafer123.angra@gmail.com>
2020-11-28 01:42:47 -0600

    Added some styles to the side bar menu, not jet finished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,13 @@ module ApplicationHelper
   def log_or_not
     log_status = []
   if logged_in?
-    log_status [0] = link_to 'Log Out', logout_path, method: :delete
-    log_status [1] = link_to 'All My Items', my_items_path(current_user.id)
-    log_status [2] = link_to 'My External Items', my_external_items_path(current_user.id)
-    log_status [3] = link_to 'All Groups', groups_path
+    log_status [0] = link_to 'Log Out', logout_path, method: :delete, :class => "list-group-item list-group-item-action bg-blue"
+    log_status [1] = link_to 'All My Items', my_items_path(current_user.id), :class => "list-group-item list-group-item-action bg-blue"
+    log_status [2] = link_to 'My External Items', my_external_items_path(current_user.id), :class => "list-group-item list-group-item-action bg-blue"
+    log_status [3] = link_to 'All Groups', groups_path, :class => "list-group-item list-group-item-action bg-blue"
   else
-    log_status [0] = link_to 'Log in', '/login'
-    log_status [1] = link_to 'Create User', new_user_path
+    log_status [0] = link_to 'Log in', '/login', :class => "list-group-item list-group-item-action bg-blue"
+    log_status [1] = link_to 'Create User', new_user_path, :class => "list-group-item list-group-item-action bg-blue"
   end
     log_status
   end
