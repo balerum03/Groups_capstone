e357f876548616f687e072561ecbbf1f1c4d263e
balerum03 <adafer123.angra@gmail.com>
2020-11-29 02:01:19 -0600

    change rootpage to groups index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :groups
   resources :items
   resources :users
-  root 'users#index'
+  root 'groups#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy', as: 'logout'
