56502a1e2a58f547a2da37b4adbcb14a2bf6bb20
balerum03 <adafer123.angra@gmail.com>
2020-12-04 23:22:14 -0600

    Added active record validations

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,5 +4,7 @@ class Group < ApplicationRecord
   has_many :items, through: :group_of_items
   has_one_attached :image, dependent: :destroy
 
+  validates :name, presence: true
+  validates :name, uniqueness: true
   scope :with_image, -> { includes(image: :blob) }
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,7 @@ class Item < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :group_of_items
   has_many :groups, through: :group_of_items
+
+  validates :name, presence: true
+  validates :amount, presence: true
 end
